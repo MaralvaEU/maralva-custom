a8b40694f9119dc1cb6ee7f6a513509e12aa6033
Juan N. Martinez <jnmaral68@gmail.com>
2026-03-16 12:29:08 +0000

    Creación rama y paso a 19

--- conta_gdigital/__manifest__.py
+++ conta_gdigital/__manifest__.py
@@ -1,6 +1,6 @@
 {
 	'name': 'Conta Gdigital',
-	'version': '18.0.1.0.0',
+	'version': '19.0.1.0.0',
 	'summary': 'Pack estándard de Contabilidad - GDigital',
 	'category': 'Accounting',
 	'author': 'Juan N. Martínez -GDigital',
