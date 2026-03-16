84b3b53747cae3b2dab87d822b86cc8ced6565b8
Juan N. Martinez <jnmaral68@gmail.com>
2026-03-16 10:45:55 +0000

    Estrucutra modulos

---
+++ conta_gdigital/__manifest__.py
@@ -0,0 +1,20 @@
+{
+	'name': 'Conta Gdigital',
+	'version': '18.0.1.0.0',
+	'summary': 'Pack estándard de Contabilidad - GDigital',
+	'category': 'Accounting',
+	'author': 'Juan N. Martínez -GDigital',
+	'website': 'https://gdigital.loc',
+	'license': 'AGPL-3',
+	'depends': [
+			'account',      		# Módulo base de Odoo para contabilidad
+			'account_edi',  		# Módulo para la generación de archivos electrónicos de contabilidad
+			
+	],
+	'data': [
+		'security/ir.model.access.csv',
+	],
+	'installable': True,
+	'auto_install': False,
+	'application': True,
+	}
